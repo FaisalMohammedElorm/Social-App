379fc065d91f7165db3bc494e638cad80874d830
FaisalMohammedElorm <mohammedelormf@gmail.com>
2025-07-02 12:49:18 +0000

    welcome punchline

--- app/Welcome.tsx
+++ app/Welcome.tsx
@@ -1,13 +1,30 @@
 import ScreenWrapper from '@/components/ScreenWrapper'
+import { wp } from '@/helpers/common'
+import { hp } from '@/helpers/common'
+import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { StatusBar, Text, View, StyleSheet } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 
 const Welcome = () => {
   return (
     <ScreenWrapper bg='#f0f0f0'>
       <StatusBar style="dark"/>
       <View style={styles.container}>
-
+        {/** Welcome Image */}
+        <Image 
+          style={styles.welcomeImage}
+          source={require('@/assets/images/welcome.png')}
+          resizeMode='contain'
+        />
+        {/** Welcome Text */}
+        <View style={{gap: 20}}>
+          <Text style={styles.title}>
+            LinkUp!
+          </Text>
+          <Text style={styles.punchLine}>
+            Where every thought finds a home, and every image tells a story.
+          </Text>
+        </View>
       </View>
     </ScreenWrapper>
   )
@@ -15,7 +32,25 @@ const Welcome = () => {
 const styles = StyleSheet.create({
   container:{
     flex: 1,
-
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    backgroundColor: 'white',
+    paddingHorizontal: wp(4),
+  },
+  welcomeImage: {
+    width: wp(100),
+    height: hp(30),
+    alignSelf: 'center',
+  },
+  title: {
+    fontSize: hp(4),
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  punchLine: {
+    fontSize: hp(3),
+    fontWeight: 'normal',
+    textAlign: 'center',
   }
 });
 export default Welcome
--- helpers/common.tsx
+++ helpers/common.tsx
@@ -1,12 +1,15 @@
 import { Dimensions } from 'react-native';
+
 const {width: deviceWidth, height: deviceHeight} = Dimensions.get('window');
 
-const hp = (percentage: number) => {
+export const hp = (percentage: number) => {
   const height = Dimensions.get('window').height;
   return (percentage * height) / 100;
 }
-const wp = (percentage: number) => {
+
+export const wp = (percentage: number) => {
   const width = Dimensions.get('window').width;
   return (percentage * width) / 100;
 }
-export { deviceWidth, deviceHeight, hp, wp };
+
+export { deviceHeight, deviceWidth };
