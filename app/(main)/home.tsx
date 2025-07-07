7c28a9df038f928800434a3460598ac346f6f0a2
FaisalMohammedElorm <mohammedelormf@gmail.com>
2025-07-07 17:48:11 +0000

    implemented supabase

--- app/(main)/home.tsx
+++ app/(main)/home.tsx
@@ -4,7 +4,7 @@ import Button from '@/components/Button'
 import { useAuth } from '@/contexts/AuthContext';
 import { supabase } from '@/lib/supabase'
 
-const home = () => {
+const Home = () => {
   const {setAuth} = useAuth()
   const onLogout = async () => {
     setAuth(null)
@@ -14,13 +14,19 @@ const home = () => {
     }
   }
   return (
-    <View>
+    <View style={styles.container}>
       <Text>home</Text>
       <Button title='Logout' onPress={onLogout} />
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 100, // Adjust this value to move content further down
+    paddingHorizontal: 20,
+  }
+})
 
-export default home
+export default Home
